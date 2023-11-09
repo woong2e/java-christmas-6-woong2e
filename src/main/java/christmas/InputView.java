7af89b3451b173e07a7a19b3d9ae4dcfaa5f9569
woong2e <tlswodnd12345@naver.com>
2023-11-09 22:36:09 +0900

    fix: 출력멘트 추가

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -9,10 +9,12 @@ public class InputView {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
-    public int askVisitDate() {
+    public int readVisitDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         while (true) {
             try {
-                int date = validate(Console.readLine());
+                String input = Console.readLine();
+                int date = validate(input);
                 return date;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
