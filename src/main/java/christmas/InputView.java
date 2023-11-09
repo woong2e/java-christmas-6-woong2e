5af7382215b8ac9219489f235aee2e4e62768956
woong2e <tlswodnd12345@naver.com>
2023-11-09 22:31:16 +0900

    feat: 날짜 입력받는 기능

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,2 +1,45 @@
-package christmas;public class InputView {
+package christmas;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    public void printHello() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public int askVisitDate() {
+        while (true) {
+            try {
+                int date = validate(Console.readLine());
+                return date;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int validate(String date) {
+        int n = validateNumber(date);
+        validateRange(n);
+        return n;
+    }
+
+    private int validateNumber(String date) {
+        try {
+            int n = Integer.parseInt(date);
+            return n;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_DATE);
+        }
+    }
+
+    private void validateRange(int date) {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException(INVALID_DATE);
+        }
+    }
+
+
 }
