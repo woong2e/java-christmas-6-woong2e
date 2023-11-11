4346a21950f33f9c22ddf29fb3649ea4705d146a
woong2e <tlswodnd12345@naver.com>
2023-11-12 01:18:21 +0900

    feat: creat steleton code

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,2 +1,45 @@
-package christmas;public class OutputView {
+package christmas;
+
+import java.util.Map;
+
+public class OutputView {
+
+    public void printEventBenefit(int date, Map<Menu, Integer> orders) {
+        printOrderMenu(orders);
+        printTotalOrderPrice();
+        printGiftMenu();
+        printBenefitList();
+        printBenefitSum();
+        printTotalOrderDiscountPrice();
+        printDecemberEventBadge();
+    }
+
+    public void printOrderMenu(Map<Menu, Integer> orders) {
+
+    }
+
+    public void printTotalOrderPrice() {
+
+    }
+
+    public void printGiftMenu() {
+
+    }
+
+    public void printBenefitList() {
+
+    }
+
+    public void printBenefitSum() {
+
+    }
+
+    public void printTotalOrderDiscountPrice() {
+
+    }
+
+    public void printDecemberEventBadge() {
+
+    }
+
 }
