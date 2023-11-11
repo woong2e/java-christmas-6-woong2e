4a57a27d8643884d36c5c444f7e6a7bf2c2f6f15
woong2e <tlswodnd12345@naver.com>
2023-11-12 00:49:57 +0900

    fix: 입력받은 주문 저장 및 입력값 유효 검사 기능 분리로 인한 코드 삭제

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,16 +1,10 @@
 package christmas;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static final String INVALID_ORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     public void printHello() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
@@ -49,108 +43,12 @@ public class InputView {
         }
     }
 
-    public Map<Menu, Integer> readMenu() {
+    public String readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        Map<Menu, Integer> order;
-        while (true) {
-            try {
-                String input = Console.readLine();
-                order = splitInput(input);
-                return order;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        String input = Console.readLine();
+        return input;
     }
 
-    private Map<Menu, Integer> splitInput(String menu) {
-        List<String> orderList = Arrays.asList(menu.split(","));
-
-        List<String> menuList = new ArrayList<>();
-        List<Integer> amountList = new ArrayList<>();
-        validateNormalInput(orderList, menuList, amountList);
-
-
-        Map<Menu, Integer> order = findMenu(menuList, amountList);
-        return order;
-    }
-
-    private void validateNormalInput(List<String> orderList, List<String> menuList, List<Integer> amountList) {
-        for (String orderMenu : orderList) {
-            try {
-                menuList.add(orderMenu.split("-")[0]);
-                amountList.add(Integer.parseInt(orderMenu.split("-")[1]));
-                if (containOnlyDrink(menuList)) {
-                    throw new IllegalArgumentException(INVALID_ORDER+1);
-                }
-            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
-                throw new IllegalArgumentException(INVALID_ORDER+2);
-            }
-        }
-    }
-
-    private boolean containOnlyDrink(List<String> menuList) {
-        List<String> drinkMenuKoreanName = Menu.getDrinkMenuKoreanName();
-
-        int n = 0;
-        for (String s : menuList) {
-            if (drinkMenuKoreanName.contains(s)) {
-                n++;
-            }
-        }
-
-        if (n == menuList.size()) {
-            return true;
-        }
-        return false;
-    }
-
-    private Map<Menu, Integer> findMenu(List<String> menuList, List<Integer> amountList) {
-        Map<Menu, Integer> order = new HashMap<>();
-        for (int i = 0; i < menuList.size(); i++) {
-            putMenuAmount(order,menuList, amountList, i);
-        }
-
-        int sum = order.values().stream().mapToInt(Integer::intValue).sum();
-        if (sum > 20) {
-            throw new IllegalArgumentException(INVALID_ORDER+3);
-        }
-
-        return order;
-    }
-
-    private void putMenuAmount(Map<Menu, Integer> order,List<String> menuList, List<Integer> amountList, int i) {
-        boolean put = false;
-        for (Menu menu : Menu.values()) {
-            if(menu.getMenuKoreanName().equals(menuList.get(i)) && amountList.get(i) > 0) {
-                validateDuplication(order, menu);
-                order.put(menu, amountList.get(i));
-                put = true;
-                break;
-            }
-        }
-        if (put) {
-            return;
-        }
-        throw new IllegalArgumentException(INVALID_ORDER+4);
-    }
-
-    private void validateDuplication(Map<Menu, Integer> order,Menu menu) {
-        if (order.containsKey(menu)) {
-            throw new IllegalArgumentException(INVALID_ORDER+5);
-        }
-    }
-
-
-
-    private Map<Menu, Integer> initOrder() {
-        Map<Menu, Integer> order = new HashMap<>();
-
-        for (Menu menu : Menu.values()) {
-            order.put(menu, 0);
-        }
-        return order;
-    }
 
 
 }
