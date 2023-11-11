716a6a2ecad512876ef0b095772c223c25844436
woong2e <tlswodnd12345@naver.com>
2023-11-12 00:20:10 +0900

    feat: menuKoreanName,price를 return하는 기능, meal이 drink인 메뉴의 koreanName을 리스트로 반환해주는 기능 추가

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -1,29 +1,53 @@
 package christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Menu {
-    MUSHROOM_SOUP("appetizer", 6_000),
-    TAPAS("appetizer", 5_500),
-    CAESAR_SALAD("appetizer", 8_000),
+    MUSHROOM_SOUP("appetizer","양송이수프", 6_000),
+    TAPAS("appetizer", "타파스", 5_500),
+    CAESAR_SALAD("appetizer", "시저샐러드" ,8_000),
 
-    T_BONE_STEAK("main", 55_000),
-    BARBECUE_RIBS("main", 54_000),
-    SEAFOOD_PASTA("main", 35_000),
-    CHRISTMAS_PASTA("main", 25_000),
+    T_BONE_STEAK("main", "티본스테이크", 55_000),
+    BARBECUE_RIBS("main", "바비큐립", 54_000),
+    SEAFOOD_PASTA("main", "해산물파스타", 35_000),
+    CHRISTMAS_PASTA("main", "크리스마스파스타", 25_000),
 
-    CHOCOLATE_CAKE("dessert", 15_000),
-    ICE_CREAM("dessert", 5_000),
+    CHOCOLATE_CAKE("dessert", "초코케이크",  15_000),
+    ICE_CREAM("dessert", "아이스크림", 5_000),
 
-    DIET_COKE("drink", 3_000),
-    RED_WINE("drink", 60_000),
-    CHAMPAGNE("drink", 25_000);
-
+    DIET_COKE("drink", "제로콜라", 3_000),
+    RED_WINE("drink", "레드와인",60_000),
+    CHAMPAGNE("drink", "샴페인", 25_000);
 
 
     private String meal;
+    private String menuKoreanName;
     private int price;
 
-    Menu(String meal, int price){
+    Menu(String meal, String menuKoreanName, int price){
         this.meal = meal;
+        this.menuKoreanName = menuKoreanName;
         this.price = price;
     }
+
+    public String getMenuKoreanName() {
+        return menuKoreanName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static List<String> getDrinkMenuKoreanName(){
+        List<String> drinkMenuKoreanName = new ArrayList<>();
+        for (Menu menu : Menu.values()) {
+            if (menu.meal.equals("drink")) {
+                drinkMenuKoreanName.add(menu.menuKoreanName);
+            }
+        }
+        return drinkMenuKoreanName;
+    }
+
+
 }
