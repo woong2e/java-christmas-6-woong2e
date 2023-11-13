464c5bbc91789597907d8a9e5cd6cde88ca09b1d
woong2e <tlswodnd12345@naver.com>
2023-11-13 13:26:51 +0900

    feat: 할인 종류 한국말 추가

--- src/main/java/christmas/Event.java
+++ src/main/java/christmas/Event.java
@@ -3,39 +3,54 @@ package christmas;
 import java.util.List;
 
 public enum Event {
-    CHRISTMAS_D_DAY_DISCOUNT(List.of(
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인",
+            List.of(
             1, 2, 3, 4, 5,
             6, 7, 8, 9, 10,
             11, 12, 13, 14, 15,
             16, 17, 18, 19, 20,
-            21, 22, 23, 24, 25), "total", 1_000),
-    WEEKDAY_DISCOUNT(List.of(
+            21, 22, 23, 24, 25), "total", -1_000),
+
+    WEEKDAY_DISCOUNT("평일 할인",
+            List.of(
             3, 4, 5, 6, 7,
             10, 11, 12, 13, 14,
             17, 18, 19, 20, 21,
-            24, 25, 26, 27, 28, 31), "dessert", 2_023),
-    WEEKEND_DISCOUNT(List.of(
+            24, 25, 26, 27, 28, 31), "dessert", -2_023),
+
+    WEEKEND_DISCOUNT("주말 할인",
+            List.of(
             1, 2, 8, 9, 15, 16,
-            22, 23, 29, 30), "main", 2_023),
-    SPECIAL_DISCOUNT(List.of(
-            3, 10, 17, 24, 25, 31), "total", 1_000),
-    PRESENTATION_EVENT(List.of(
+            22, 23, 29, 30), "main", -2_023),
+
+    SPECIAL_DISCOUNT("특별 할인",
+            List.of(
+            3, 10, 17, 24, 25, 31), "total", -1_000),
+
+    PRESENTATION_EVENT("증정 이벤트",
+            List.of(
             1, 2, 3, 4, 5,
             6, 7, 8, 9, 10,
             11, 12, 13, 14, 15,
             16, 17, 18, 19, 20,
             21, 22, 23, 24, 25,
             26, 27, 28, 29, 30,
-            31), "drink", 25_000);
+            31), "drink", -25_000);
 
+    private String discountKind;
     private List<Integer> date;
     private String item;
     private int benefit;
 
-    Event(List<Integer> date, String item, int benefit) {
+    Event(String discountKind, List<Integer> date, String item, int benefit) {
+        this.discountKind = discountKind;
         this.date = date;
         this.item = item;
         this.benefit = benefit;
+    }
+
+    public String getDiscountKind() {
+        return discountKind;
     }
 
     public List<Integer> getDate() {
