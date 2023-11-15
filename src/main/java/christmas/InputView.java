d5eee5596807ab32362065dba73713cd35e8f9cc
woong2e <tlswodnd12345@naver.com>
2023-11-15 12:10:18 +0900

    chore: 줄간격 수정

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,11 +1,8 @@
 package christmas;
 
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-
     public int readVisitDate() {
         try {
             System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
