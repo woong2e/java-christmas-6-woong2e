0f42c3bd3340e2b6a34cae87b6d634cf2aaf3472
woong2e <tlswodnd12345@naver.com>
2023-11-15 13:13:46 +0900

    chore: 필요없는 라인 삭제

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -8,8 +8,7 @@ public class InputView {
             System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
             System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
             String input = Console.readLine();
-            int date = validateDate(input);
-            return date;
+            return validateDate(input);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
@@ -37,7 +36,6 @@ public class InputView {
 
     public String readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String input = Console.readLine();
-        return input;
+        return Console.readLine();
     }
 }
